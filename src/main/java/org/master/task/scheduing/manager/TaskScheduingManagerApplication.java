8a04fe023f095f48e65cd9140f3141a2d2566e7d
Master-Alpah <3030653596@qq.com>
2024-08-21 11:56:51 +0800

    1

---
+++ src/main/java/org/master/task/scheduing/manager/TaskScheduingManagerApplication.java
@@ -0,0 +1,13 @@
+package org.master.task.scheduing.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskScheduingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskScheduingManagerApplication.class, args);
+    }
+
+}
